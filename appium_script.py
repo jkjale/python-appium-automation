1a1f3c70aa4757382bd491d27c9862f2467ed533
Jake Lee <jkjalee@gmail.com>
2025-03-30 16:59:06 -0700

    Set newCommandTimeout to 5000

--- appium_script.py
+++ appium_script.py
@@ -10,7 +10,7 @@ desired_capabilities = {
     "appActivity": "org.wikipedia.main.MainActivity",
     "appPackage": "org.wikipedia",
     "app": "/Users/jakelee/Desktop/QA/python-appium-automation/mobile_app/uptodown-org.wikipedia.apk",
-    "newCommandTimeout": 1000
+    "newCommandTimeout": 5000
 }
 
 appium_server_url = 'http://localhost:4723'
@@ -19,15 +19,15 @@ capabilities_options = UiAutomator2Options().load_capabilities(desired_capabilit
 driver = webdriver.Remote(appium_server_url, options=capabilities_options)
 driver.implicitly_wait(5)
 
-# driver.find_element(AppiumBy.ID, 'org.wikipedia:id/fragment_onboarding_skip_button').click()
-#
-# driver.find_element(AppiumBy.XPATH, "//*[@content-desc='Search Wikipedia']").click()
-#
-# driver.find_element(AppiumBy.ID, 'org.wikipedia:id/search_src_text').send_keys('Python (programming language)')
-#
-# expected_result = 'Python (programming language)'
-# actual_result = driver.find_element(AppiumBy.ID, 'org.wikipedia:id/page_list_item_title').text
-#
-# assert actual_result == expected_result, f'Expected {expected_result} did not match actual {actual_result}'
-#
-# driver.quit()
+driver.find_element(AppiumBy.ID, 'org.wikipedia:id/fragment_onboarding_skip_button').click()
+
+driver.find_element(AppiumBy.XPATH, '//android.widget.TextView[@text="Search Wikipedia"]').click()
+
+driver.find_element(AppiumBy.ID, 'org.wikipedia:id/search_src_text').send_keys('Python (programming language)')
+
+actual_text = driver.find_element(AppiumBy.XPATH, '//*[@resource-id="org.wikipedia:id/page_list_item_title" and @text="Python (programming language)"]').text
+expected_text = 'Python (programming language)'
+
+assert actual_text == expected_text, f'Expected {actual_text} but got {expected_text}'
+
+driver.quit()
